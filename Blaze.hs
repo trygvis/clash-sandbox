72cff86ebcd02432c8b51939877260a481576de3
Trygve Laugstøl <trygvis@inamo.no>
2011-05-09 21:32:07 +0200

    o Using an Vector instead of MemState to store the registers and everything just works again.

--- Blaze.hs
+++ Blaze.hs
@@ -1,6 +1,6 @@
 module Blaze where
 
-import CLasH.HardwareTypes hiding (readBlockRAM, writeBlockRAM)
+import CLasH.HardwareTypes
 
 type Word = Signed D4
 type RegisterIndex = Index D4
@@ -10,42 +10,42 @@ data Instruction =
   | In RegisterIndex
   | Out RegisterIndex
   | Add RegisterIndex RegisterIndex RegisterIndex
+  | Sub RegisterIndex RegisterIndex RegisterIndex
 
 data BlazeR = R {
-    registers :: MemState D4 Word
+    registers :: Vector D4 Word
   }
 
 type BlazeS = State BlazeR
 
 blazeInit :: BlazeR
-blazeInit = R { registers = State (vcopy 0) }
+blazeInit = R { registers = vcopy 0 }
 
 blaze :: BlazeS -> (Instruction, Word) -> (BlazeS, Word)
 blaze (State s) (instruction, inWord) = (State s', outWord)
   where
-    -- | Returns the value of the RAM at address 'addr'
-    readBlockRAM mem addr = a
-      where (_, a) = blockRAM mem 0 addr addr False
-    -- | Updates the value of the RAM at address 'addr' with 'data_in'
-    writeBlockRAM mem data_in addr = mem'
-      where (mem', _) = blockRAM mem data_in addr addr True
     regs = registers s
     (s', outWord) = case instruction of
       Nop -> (s, 0)
       In index -> (R { registers = regs' }, 0) 
-        where (regs', _) = blockRAM regs inWord 0 index True
+        where regs' = vreplace regs index inWord
       Out index -> (s, outWord) 
-        where (_, outWord) = blockRAM regs 0 index 0 False
+        where outWord = regs!index
       Add x y z -> (R { registers = regs' }, 0) 
         where
-          (_, a) = blockRAM regs 0 x 0 False
-          (_, b) = blockRAM regs 0 y 0 False
-          (regs', _) = blockRAM regs (a + b) 0 z True
+          a = regs!x
+          b = regs!y
+          regs' = vreplace regs z (a + b)
+      Sub x y z -> (R { registers = regs' }, 0) 
+        where
+          a = regs!x
+          b = regs!y
+          regs' = vreplace regs z (a - b)
 
-{-# ANN exec TopEntity #-}
--- exec = proc (instruction, word) -> do
-exec = blaze ^^^ blazeInit
+{-# ANN blazeTop TopEntity #-}
+blazeTop = blaze ^^^ blazeInit
 
+{-# ANN program1 TestInput #-}
 program1 :: [(Instruction, Word)]
 program1 = [
     (In  0,  1)
@@ -54,6 +54,6 @@ program1 = [
   , (Out 2, 0)
   ]
 
-sim p = simulate (blaze ^^^ blazeInit) p
+sim p = simulate blazeTop p
 
 -- vim: set ts=8 sw=2 sts=2 expandtab:
